0c0f13f7b1e7245c3b1bbf1f5c5d240d93e5a1b5
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-12-12 14:10:58 -0800

    Components Done

---
+++ src/components/AddedFeatures.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AddedFeature from './AddedFeature';
+import { connect } from "react-redux";
+
+const AddedFeatures = ({ car, removeFeature }) => {
+  return (
+    <div className="content">
+      <h6>Added features:</h6>
+      {car.features.length ? (
+        <ol type="1">
+          {car.features.map(item => (
+            <AddedFeature 
+            key={item.id} 
+            feature={item} 
+            removeFeature={removeFeature}
+            />
+          ))}
+        </ol>
+      ) : (
+        <p>You can purchase items from the store.</p>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    car: state.car
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(AddedFeatures);
