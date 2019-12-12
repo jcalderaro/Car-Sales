5d22a2bb6da53975e10f526b5ccafe011d2e9e3d
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-12-12 14:07:39 -0800

    Index

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//imports
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bulma/css/bulma.css';
+import './styles.scss';
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { reducer } from "./reducers/reducer";
+
+const rootElement = document.getElementById('root');
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+
+<Provider store={store}>
+    <App />
+</Provider>
+
+, rootElement);
